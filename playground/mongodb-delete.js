de0af1192a894e27e6f43df4a5b16fe37524cb1d
bar2016 <barstrolovich1@gmail.com>
2018-12-09 16:32:41 +0200

    Updated Delete Script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -11,6 +11,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
+  //delete many
+  // db.collection('Todos').deleteMany({text: "Eat launch"}).then((result) =>{
+  //   console.log(result);
+  // });
 
+  //Delete One
+  // db.collection('Todos').deleteOne({text: 'Take the dog out'}).then((res)=>{
+  //   console.log(res);
+  // });
+
+  //FindOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((res)=>{
+  //   console.log(res);
+  // });
+
+  //Challenge
+  // db.collection('Users').deleteMany({name: "Bar"}).then((res)=>{
+  //   console.log(res);
+  // });
+  db.collection('Users').findOneAndDelete({_id: new ObjectID("5c0945f61335152060a33f1f")}).then((res)=>{
+    console.log(res);
+  });
   // client.close();
 });
